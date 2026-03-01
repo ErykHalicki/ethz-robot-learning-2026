798db29da3f05fd29dc13be6780b2de45b29459e
ErykHalicki <erykhalicki0@gmail.com>
2026-03-01 16:25:41 +0100

    nicer plots

--- hw1_pytorch_tutorial/src/visualize.py
+++ hw1_pytorch_tutorial/src/visualize.py
@@ -22,29 +22,41 @@ processsed_training_results = {}
 for k in training_result_dict:
     processsed_training_results[k] = create_arrays_from_list_of_dicts(training_result_dict[k])
 
-print(processsed_training_results)
 
-'''
 
 # loss vs batch
 ax = plt.axes()
-ff_line, = ax.plot(training_result_dict["ff"]["train_loss"])
-reglu_line, = ax.plot(training_result_dict["reglu"]["train_loss"])
-geglu_line, = ax.plot(training_result_dict["geglu"]["train_loss"])
-ax.legend([ff_line, reglu_line, geglu_line],["ff", "reglu", "geglu"])
-plt.title("Training Loss vs Batch")
+lines=[]
+for k in processsed_training_results:
+    data = processsed_training_results[k]["train_loss"]
+    x = np.arange(data.shape[1])
+    mean = np.mean(data, axis=0)
+    std = np.std(data, axis=0)
+    line, = ax.plot(x, mean)
+    ax.fill_between(x, mean - std, mean + std, alpha=0.2, color=line.get_color())
+    lines.append(line)
+
+ax.legend(lines,processsed_training_results.keys())
+plt.title("Training Loss vs Batch\n10 Run Mean and Std Dev")
 ax.set_xlabel("Batch")
 ax.set_ylabel("Training Loss")
 plt.show()
 
 # test_accuracy vs epoch
 ax = plt.axes()
-ff_line, = ax.plot(training_result_dict["ff"]["test_accuracy"], marker='o')
-reglu_line, = ax.plot(training_result_dict["reglu"]["test_accuracy"], marker='o')
-geglu_line, = ax.plot(training_result_dict["geglu"]["test_accuracy"], marker='o')
-ax.legend([ff_line, reglu_line, geglu_line],["ff", "reglu", "geglu"])
-plt.title("Test Accuracy vs Batch")
-ax.set_xlabel("Batch")
+lines=[]
+for k in processsed_training_results:
+    data = processsed_training_results[k]["test_accuracy"]
+    x = np.arange(data.shape[1])
+    mean = np.mean(data, axis=0)
+    std = np.std(data, axis=0)
+    line, = ax.plot(x, mean)
+    ax.fill_between(x, mean - std, mean + std, alpha=0.2, color=line.get_color())
+    lines.append(line)
+
+ax.legend(lines, processsed_training_results.keys())
+plt.title("Test Accuracy vs Epoch\n10 Run Mean and Std Dev")
+ax.set_xlabel("Epoch")
 ax.set_ylabel("Test Accuracy (%)")
 plt.show()
 
@@ -56,5 +68,3 @@ for thresh in threshes:
                 print(f"{kind} reached loss {thresh} at batch {i}")
                 break
     print("---------------")
-
-'''
