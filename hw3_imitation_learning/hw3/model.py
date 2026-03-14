be598956d76a84253489eaf82afe3cb56d3d772a
ErykHalicki <erykhalicki0@gmail.com>
2026-03-14 15:28:16 +0100

    bruh

--- hw3_imitation_learning/hw3/model.py
+++ hw3_imitation_learning/hw3/model.py
@@ -63,7 +63,7 @@ class ObstaclePolicy(BasePolicy):
         self.gripper_output_layer = nn.Linear(d_model, self.gripper_action_dim*self.chunk_size)
         self.dropout = torch.nn.Dropout(p=0.15)
 
-        zero_movement_weight = 0.035
+        zero_movement_weight = 0.015
         self.log_var_ee = nn.Parameter(torch.zeros(1))
         self.log_var_gripper = nn.Parameter(torch.zeros(1))
         ee_ce_weights = torch.zeros([7])
@@ -124,7 +124,7 @@ class ObstaclePolicy(BasePolicy):
         state: torch.Tensor,
     ) -> torch.Tensor:
         self.eval()
-        temp = 0.3
+        temp = 1.2
         with torch.no_grad():
             action_logits = self.forward(state)
             #action_logits["ee"][:, :, 0] /= 5
