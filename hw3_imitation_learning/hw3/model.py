0dddcb9bef1a1d5bc76feb52d514dbfcce9ddae5
ErykHalicki <erykhalicki0@gmail.com>
2026-03-14 15:08:06 +0100

    im a bit

--- hw3_imitation_learning/hw3/model.py
+++ hw3_imitation_learning/hw3/model.py
@@ -49,7 +49,7 @@ class ObstaclePolicy(BasePolicy):
     ) -> None:
         super().__init__(*args, **kwargs)
         # model size parameters
-        self.gripper_action_dim = 25
+        self.gripper_action_dim = 15
         self.ee_action_dim = 7 #[0, +x, +y, +z, -x, -y, -z]
         self.depth = depth 
         self.d_model = d_model
