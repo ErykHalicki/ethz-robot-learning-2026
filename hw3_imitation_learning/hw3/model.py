7a9a226f9fb81f477ba8609a218bdf274c6d0037
ErykHalicki <erykhalicki0@gmail.com>
2026-03-14 15:32:55 +0100

    0

--- hw3_imitation_learning/hw3/model.py
+++ hw3_imitation_learning/hw3/model.py
@@ -81,7 +81,7 @@ class ObstaclePolicy(BasePolicy):
                                           [-1.,0.,0.],  # -x
                                           [0.,-1.,0.],  # -y
                                           [0.,0.,-1.]]) # -z
-        self.ee_translation_per_step = 0.033
+        self.ee_translation_per_step = 0.0033
         
         self.register_buffer('gripper_centers', (gripper_bounds[:-1] + gripper_bounds[1:]) / 2)
         self.register_buffer('gripper_bounds', gripper_bounds)
